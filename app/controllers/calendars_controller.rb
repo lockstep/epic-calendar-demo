e33613c8da4c3652a8abdd511484bdb4b1559750
Pariwat Chaichomphu <cpariwat@me.com>
2017-09-26 22:35:03 +0700

    add refresh token when call api

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,34 +1,16 @@
 # frozen_string_literal: true
 
 class CalendarsController < ApplicationController
-  skip_before_action :authenticate_user!
-
-  def redirect
-    client = Signet::OAuth2::Client.new(client_options)
-
-    redirect_to client.authorization_uri.to_s
-  end
-
-  def callback
-    client = Signet::OAuth2::Client.new(client_options)
-    client.code = params[:code]
-
-    response = client.fetch_access_token!
-
-    session[:authorization] = response
-
-    redirect_to calendars_url
-  end
 
   def calendars
     client = Signet::OAuth2::Client.new(client_options)
-    client.update!(session[:authorization])
+    client.fetch_access_token!
 
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = client
 
     @calendar_list = service.list_calendar_lists
-  rescue Google::Apis::ClientError
+  rescue Google::Apis::AuthorizationError
     response = client.refresh!
 
     session[:authorization] = session[:authorization].merge(response)
@@ -38,7 +20,7 @@ class CalendarsController < ApplicationController
 
   def events
     client = Signet::OAuth2::Client.new(client_options)
-    client.update!(session[:authorization])
+    client.fetch_access_token!
 
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = client
@@ -55,7 +37,8 @@ class CalendarsController < ApplicationController
       authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
       token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
       scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
-      redirect_uri: callback_url
+      redirect_uri: user_google_oauth2_omniauth_authorize_url,
+      refresh_token: current_user.refresh_token
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions', omniauth_callbacks: 'callbacks' }
 
   unauthenticated :user do
     get '/redirect', to: 'calendars#redirect', as: 'redirect'
-    get '/users/auth/google_oauth2/callback', to: 'calendars#callback', as: 'callback'
+    get '/callback', to: 'calendars#callback', as: 'callback'
+
+    root to: 'pages#landing'
+  end
+
+  authenticated :user do
     get '/calendars', to: 'calendars#calendars', as: 'calendars'
     get '/events/:calendar_id', to: 'calendars#events', as: 'events', calendar_id: /[^\/]+/
 
-    root to: 'pages#landing'
+    root to: 'calendars#calendars'
   end
 end
