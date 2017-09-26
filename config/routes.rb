a6a7ce197d4d4c8a7098383c468427e746bfe82b
Pariwat Chaichomphu <cpariwat@me.com>
2017-09-26 22:14:41 +0700

    change route for callback

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   unauthenticated :user do
     get '/redirect', to: 'calendars#redirect', as: 'redirect'
-    get '/callback', to: 'calendars#callback', as: 'callback'
+    get '/users/auth/google_oauth2/callback', to: 'calendars#callback', as: 'callback'
     get '/calendars', to: 'calendars#calendars', as: 'calendars'
     get '/events/:calendar_id', to: 'calendars#events', as: 'events', calendar_id: /[^\/]+/
 
